b558b9b33bd17bf547c410725c975c079dd334f5
Diego Souza <diego@diegosouzati.com.br>
2021-07-02 13:51:11 -0300

    conclusão parte desktop e inicio da responsividade

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -6,5 +6,30 @@ window.addEventListener('load', function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-    });
+        responsive: [
+            {
+              breakpoint: 1125,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 1030,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 670,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });          
 });
